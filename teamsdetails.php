62f0da0d0770fdb9a00ea1f55928545ed4a86e5f
alshoikan <34734773+alshoikan@users.noreply.github.com>
2021-06-19 07:34:15 +0300

    Create teamsdetails.php

---
+++ teamsdetails.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">الرجوع إلى الصفحة الرئيسية</a>
+        
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <table class="team-table">
+
+                    <tr>
+                        <th>اسم الفريق</th>
+                        <th>اسم الفريق</th>
+                    </tr>
+
+                    <tr>
+                        <td class="orange">
+                        <a target="_blank" href="assets/pic/alnasserclub.png">
+                        <img src="assets/pic/alnasserclub.png">
+                        </a>
+                    <ul>
+                        <li>نادي النصر</li>
+                        <li> أحد الأندية المشهورة </li>
+                        <li> تأسس عام 1955 ميلادي </li>
+                    </ul>
+                </td>
+    
+
+
+
+                <td class="orange">
+                     <a target="_blank" href="assets/pic/alittihadclub.png">
+                        <img src="assets/pic/alittihadclub.png">
+                    </a>
+                        <ul>
+                            <li>نادي الإتحاد</li>
+                            <li> أحد الأندية المشهورة </li>
+                            <li> تأسس عام 1927 ميلادي </li>
+                        </ul>
+                </td>
+    
+            </tr>
+
+            <tr>
+                <td>
+                    <a target="_blank" href="assets/pic/alhillalclub.jfif">
+                    <img src="assets/pic/alhillalclub.jfif">
+                    </a>
+                    <ul>
+                        <li>نادي الهلال</li>
+                        <li> أحد الأندية المشهورة </li>
+                        <li> تأسس عام 1957 ميلادي </li>
+                    </ul>
+                </td>
+    
+
+
+                <td>
+                    <a target="_blank" href="assets/pic/alahliclub.png">
+                    <img src="assets/pic/alahliclub.png">
+
+                    </a>
+                    <ul>
+                        <li>النادي الأهلي</li>
+                        <li> أحد الأندية المشهورة </li>
+                        <li> تأسس عام 1937 ميلادي </li>
+                    </ul>
+                </td>
+    
+            </tr>
+
+
+            </table>
+                
+            </div>
+            
+        </div>
+    </div>
+
+    
+</body>
