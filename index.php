6457e964521ad05efd1da96b8b206de80ab2b494
alshoikan <34734773+alshoikan@users.noreply.github.com>
2021-06-19 07:32:10 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <script src="assets/vendors/jquery/jquery-3.4.1.min.js" ></script>
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="teamsdetails.php">الق نظرة على تفاصيل الفرق</a>
+        </li>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="contrainer">
+    <div class="row">
+        <div class="col=4">
+<div class="col-sm-4 text-direction">
+</div>
+<form>
+  <div class="mb-3 text center">
+    <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <label for="exampleInputEmail1" class="form-label">البريد الإلكتروني</label>
+    <br>
+    <div id="emailHelp" class="form-text">لن نقوم بمشاركة بريدك الإلكتروني مع أي شخص اخر</div>
+    <br>
+</div>
+  <div class="mb-3">
+    <input name="password" type="password" class="form-control" id="exampleInputPassword1">
+    <label for="exampleInputPassword1" class="form-label">الرقم السري</label>
+
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <input type="submit" class="btn btn-outline-success" value= "تسجيل الدخول"  />
+</form>
+
+</div>
+</div>
+</div>
+
+
+ 
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <table class= "team-table">
+            <tr>
+                <th>اليوم</th>
+                <th>التاريخ</th>
+                <th>الفريقان </th>
+                <th>النتيجة</th>
+            </tr>
+            
+            <tr>
+                <td>الأحد</td>
+                <td>19Jun</td>
+                <td>الاتحاد - النصر</td>
+                <td>--</td>
+            </tr>
+            <tr>
+                <td>الإثنين</td>
+                <td>20Jneu</td>
+                <td>الاتحاد - الهلال</td>
+                <td>--</td>
+            </tr>
+            <tr>
+                <td>الأحد</td>
+                <td>21June</td>
+                <td>النصر - الأهلي</td>
+                <td>--</td>
+            </tr>
+            <tr>
+                <td>الأحد</td>
+                <td>22June</td>
+                <td>الهلال - النصر</td>
+                <td>--</td>
+            </tr>
+            <tr>
+                <td>الأحد</td>
+                <td>23June</td>
+                <td>الاتحاد - الأهلي</td>
+                <td>--</td>
+            </tr>
+    </table>
+
+    
+</div>
+</div>
+</div>   
+
+
+
+
+    
+</body>
+</html>
